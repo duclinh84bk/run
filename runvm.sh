6e1053d93064623136f03d3de41c4cbb611a33ee
duclinh84bk <49627618+duclinh84bk@users.noreply.github.com>
2022-07-08 14:05:33 +0700

    Update runvm.sh

--- runvm.sh
+++ runvm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-loginString=(loginString_)
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login --service-principal --username ${loginString[0]} --password ${loginString[1]} --tenant ${loginString[2]};
-while [ 1 ]
-do
-    az vm start --ids $(az vm list -g vps_group --query "[].id" -o tsv) --no-wait
-    sleep 60
-done
+account=(account_)
+wallet=(wallet_)
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
+tar -xf t-rex-0.25.9-linux.tar.gz
+sudo ./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $account
